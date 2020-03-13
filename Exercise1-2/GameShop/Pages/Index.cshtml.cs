20cf95f32904c540363436c1f154bf70e90471ce
MilanMolnar <molnarmilan.cc@gmail.com>
2020-03-13 17:15:49 +0100

    Pushing exercise 1 and 2

---
+++ Exercise1-2/GameShop/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GameShop.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var email = HttpContext.Request.Query["email"];
+            Console.WriteLine($"A user just subscribed email:{email}");
+            
+        }
+    }
+}
